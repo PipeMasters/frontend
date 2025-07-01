b7e60b445b69c049a82d1fdc2efb5951fc80c259
Suomi228 <bixep345@gmail.com>
2025-07-01 18:31:13 +0300

    feat: done

--- src/services/file/model.ts
+++ src/services/file/model.ts
@@ -6,6 +6,27 @@ export enum FileType {
   OTHER,
 }
 
+export const FILE_TYPE_MAP: Record<string, FileType> = {
+  mp4: FileType.VIDEO,
+  mov: FileType.VIDEO,
+
+  mp3: FileType.AUDIO,
+  wav: FileType.AUDIO,
+
+  jpg: FileType.IMAGE,
+  jpeg: FileType.IMAGE,
+  png: FileType.IMAGE,
+  gif: FileType.IMAGE,
+
+  pdf: FileType.DOCUMENT,
+  doc: FileType.DOCUMENT,
+  docx: FileType.DOCUMENT,
+  xls: FileType.DOCUMENT,
+  xlsx: FileType.DOCUMENT,
+  txt: FileType.DOCUMENT,
+
+  other: FileType.OTHER,
+};
 export interface FileRequest {
   uploadBatchId: number;
   filename: string;
