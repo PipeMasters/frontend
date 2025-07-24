8cc73575d72ccf630a297423decf7fc891d0c569
Andrey <79145526+yobaNGE@users.noreply.github.com>
2025-07-24 06:52:15 +0300

    Update $fileId.tsx

--- src/routes/overview/$fileId.tsx
+++ src/routes/overview/$fileId.tsx
@@ -250,17 +250,17 @@ function OverviewComponent() {
     },
   ];
 
-  // const downloadFile = async (url: string, filename: string) => {
-  //   const response = await fetch(url);
-  //   const blob = await response.blob();
-  //   const link = document.createElement("a");
-  //   link.href = URL.createObjectURL(blob);
-  //   link.download = filename;
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   link.remove();
-  //   URL.revokeObjectURL(link.href);
-  // };
+   const downloadFile = async (url: string, filename: string) => {
+     const response = await fetch(url);
+     const blob = await response.blob();
+     const link = document.createElement("a");
+     link.href = URL.createObjectURL(blob);
+     link.download = filename;
+     document.body.appendChild(link);
+     link.click();
+     link.remove();
+     URL.revokeObjectURL(link.href);
+   };
 
   return (
     <div className="flex flex-col p-10 pt-0 gap-8">
